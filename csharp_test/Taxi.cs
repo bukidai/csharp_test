a345a1641e163bc8f93f2f195cc7959160bb4382
bukidai <k022c0054@g.neec.ac.jp>
2023-11-23 22:37:18 +0900

    ドキュメント作成

---
+++ csharp_test/Taxi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace csharp_test
+{
+    /// <summary>
+    /// タクシークラス
+    /// </summary>
+    internal class Taxi : Car
+    {
+        public int Fare { get; set; }
+        public int Fee { get; set; }
+
+        public Taxi(int fare, int fee, int speed, string name) : base(name, 1200, speed)
+        {
+            Fare = fare;
+            Fee = fee;
+        }
+
+        public override void Run()
+        {
+            Console.WriteLine("タクシーが走ります");
+        }
+    }
+}
